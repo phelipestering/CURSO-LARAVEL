e7f4d3fd4b7ef3c656bb60010d91fd30175be7ab
Phelipe Stering <phelipe.stering@hotmail.com>
2021-09-24 23:22:16 -0300

    Iniciando estudo de Factory

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+    }
+}
+
+/*
+        User::create([
+            'name' => 'Phelipe Stering Lima',
+            'Email' => 'phelipe.stering@hotmail.com',
+            'password' => bcrypt('123456'),
+
+       ]);
+       */
+
+
+  
